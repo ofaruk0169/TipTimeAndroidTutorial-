d29067cc86508c2ff8e2984347c4b2e99fbcd298
Omare Faruk <ofaruk0169@gmail.com>
2023-09-12 11:38:43 +0100

    created more instrumentation tests for different scenarios, including when the round up button is not pressed

--- app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
+++ app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
@@ -29,4 +29,45 @@ class CalculatorTests {
         onView(withId(R.id.tip_result))
             .check(matches(withText(containsString("$10.00"))))
     }
+
+    @Test
+    fun calculate_18_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+
+        onView(withId(R.id.option_eighteen_percent)).perform(click())
+
+        onView(withId(R.id.calculate_button)).perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$9.00"))))
+    }
+
+    @Test
+    fun calculate_15_percent_tip() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+
+        onView(withId(R.id.option_fifteen_percent)).perform(click())
+
+        onView(withId(R.id.calculate_button)).perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$8.00"))))
+    }
+
+    @Test
+    fun calculate_15_percent_tip_no_round() {
+        onView(withId(R.id.cost_of_service_edit_text))
+            .perform(typeText("50.00"))
+
+        onView(withId(R.id.option_fifteen_percent)).perform(click())
+
+        onView(withId(R.id.round_up_switch)).perform(click())
+
+        onView(withId(R.id.calculate_button)).perform(click())
+
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$7.50"))))
+    }
 }
